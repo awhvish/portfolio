de03a1171f50fe50aeea6b52ec13a22b6cfe965a
root <high.avis.lowha@gmail.com>
2024-09-10 02:48:43 +0530

    mob nav update 1

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -19,14 +19,13 @@ const MobileNav = () => {
                 <SheetContent>
                     {links.map((link, index) => {
                         return (
-                            <div className='flex items-center pt-5'>
+                            <div className='flex items-center pt-5' key={index}>
                                 <Link 
-                                    href={link.path} key={index} 
+                                    href={link.path} 
                                     className={`${
                                     pathname == link.path ? 'text-accent border-b-2 border-accent' : 'text-white'}
                                     hover:text-accent transition-all`}>{link.name}
                                 </Link>
-                                <br></br>
                             </div>
                         )
                     })}
